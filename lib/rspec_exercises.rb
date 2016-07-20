2fd56f894c5bd60a05dc014eea177d93ed4484b5
App Academy Student <student@appacademy.io>
2016-07-20 10:51:31 -0700

    completed two_sum and spec

--- lib/rspec_exercises.rb
+++ lib/rspec_exercises.rb
@@ -1,7 +1,22 @@
-def my_uniq(array)
-  new_array = []
-  array.each do |el|
-    new_array << el unless new_array.include?(el)
+
+class Array
+  def my_uniq
+    new_array = []
+    each do |el|
+      new_array << el unless new_array.include?(el)
+    end
+    new_array
   end
-  new_array
+
+  def two_sum
+    indices = []
+    each_with_index do |el, i|
+      self.each_with_index do |el2, j|
+        next if i == j || i > j
+        indices << [i, j] if el + el2 == 0
+      end
+    end
+
+    indices
+  end
 end
--- spec/rspec_exercises_spec.rb
+++ spec/rspec_exercises_spec.rb
@@ -1,8 +1,14 @@
 require 'rspec'
 require 'rspec_exercises'
 
-describe '#my_uniq'do
+describe 'Array::my_uniq' do
   it 'removes all duplicated elements' do
-    expect(my_uniq([1,2,2,2,2,3,4])).to eq([1,2,3,4])
+    expect([1,2,2,2,2,3,4].my_uniq).to eq([1,2,3,4])
+  end
+end
+
+describe "Array::two_sum" do
+  it "returns an array with indices of pairs that sum to zero in order" do
+    expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
   end
 end
