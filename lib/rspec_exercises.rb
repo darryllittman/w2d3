96be796a05a7d8227ce2628c21be2984ad4b613b
App Academy Student <student@appacademy.io>
2016-07-20 11:21:43 -0700

    stock_prices method and spec

--- lib/rspec_exercises.rb
+++ lib/rspec_exercises.rb
@@ -19,4 +19,32 @@ class Array
 
     indices
   end
+
+  def my_transpose
+    new_arr = []
+    idx = 0
+    while idx < self.size
+      new_arr[idx] = []
+      idx2 = 0
+      while idx2 < self.size
+        new_arr[idx] << self[idx2][idx]
+        idx2 += 1
+      end
+      idx += 1
+    end
+    new_arr
+  end
+end
+
+def stock_prices(stock_info)
+  stock_combos = []
+  stock_info.each_with_index do |price, day|
+    stock_info.each_with_index do |price2, day2|
+      next if day >= day2
+      stock_combos << [price, price2, [day, day2]]
+    end
+  end
+
+  stock_combos.sort_by {|price1, price2, options| (price2 - price1).abs  }.last.last
+
 end
--- spec/rspec_exercises_spec.rb
+++ spec/rspec_exercises_spec.rb
@@ -12,3 +12,22 @@ describe "Array::two_sum" do
     expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
   end
 end
+
+describe "Array::my_transpose" do
+  it "transposes an array" do
+    expect([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ].my_transpose).to eq([[0, 3, 6],
+             [1, 4, 7],
+             [2, 5, 8]])
+  end
+end
+
+describe '#stock_prices' do
+  it 'finds most profitable pair' do
+    expect(stock_prices([0.01, 0.52, 1.08, 0.08, 1.04])).to eq([0, 2])
+    expect(stock_prices([5.3, 1, 7.8, 45, 46])).to eq([1, 4])
+  end
+end
