5f138e9d088333c9238f784a52119e3623d24745
App Academy Student <student@appacademy.io>
2016-07-20 13:39:46 -0700

    create card class and spec file

---
+++ poker/spec/card_spec.rb
@@ -0,0 +1,9 @@
+describe Card do
+  let(:card) { Card.new(8, :h) }
+  describe '#initialize' do
+    it "set to an ititial value" do
+      expect(card.value).to eq(8)
+      expect(card.suit).to eq(:h)
+    end
+  end
+end
